8737208cdf3cba77366fc9156e40594cc35ca29b
Whefert <jeffersondaley@gmail.com>
2023-12-09 20:57:03 +0000

    Add data persistence on front-end

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,18 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 today = dayjs().format("dddd, MMMM Do");
 
 calendar = $("#calendar");
-
-$("#currentDay").text(today);
+tasks = $("#currentDay").text(today);
+taskArray = [];
 
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function createCalendar() {
-  //loop through hours array and create corresponding elements
+  //loop through the hours array and create corresponding elements
   for (let i = 0; i < hours.length; i++) {
     const element = hours[i];
     row = $("<div>").addClass("row");
-    //create time column
-    // TODO: Put time text in a paragaph
 
+    //create time column
     timeDiv = $("<div>").addClass("col-1 hour");
     time = $("<p>").addClass("time-block mt-3");
     time.text(findHour(hours[i]));
@@ -26,8 +25,6 @@ function createCalendar() {
     task = $("<textarea>");
     task.addClass("w-100");
     task.attr("data-hour", `${hours[i]}`);
-
-    //if time has passed then
     taskDiv.append(task);
 
     //create save button
@@ -46,7 +43,7 @@ function createCalendar() {
   }
 }
 
-//if time has passed, change background color to gret
+//if time has passed, if it is in the present or future, add the corresponding css
 function isPassed(currentHour) {
   if (dayjs().format("H") > currentHour) {
     return "past";
@@ -57,7 +54,7 @@ function isPassed(currentHour) {
   }
 }
 
-//calculates the hour based on a 12 hour clock and returns the current hour
+//calculate the hour based on a 12 hour clock and return the current hour
 function findHour(hour) {
   if (hour == 12) {
     return `${12}PM`;
@@ -67,15 +64,36 @@ function findHour(hour) {
   return `${hour}AM`;
 }
 
+//populate rows with existing data in local storage
+function populateCalendar() {
+  //for each item in local storage try to find which hour it is associated with
+  for (let i = 0; i < hours.length; i++) {
+    let task = localStorage.getItem(`task-${hours[i]}`);
+    if (task != null) {
+      task = JSON.parse(task);
+      taskArray.push(task);
+    }
+  }
+  //find hour for each task and the corresponding text area
+  taskArray.forEach((task) => {
+    $(`textarea[data-hour = ${task.hour}]`).val(task.task);
+  });
+}
+
 createCalendar();
-
+populateCalendar();
+//add listener to all save buttons
 $("button").on("click", (e) => {
+  //find hour for the row of the clicked button/icon
   hourToSave = e.target.getAttribute("data-hour");
-
-  //save value of selected textarea
+  //save value of the selected textarea
   textToSave = $(`textarea[data-hour = ${hourToSave}]`).val();
   if (textToSave.trim().length === 0) {
     alert("Cannot save an empty task");
   }
-  localStorage.setItem(hourToSave, textToSave);
+  //save the input text to local storage
+  localStorage.setItem(
+    `task-${hourToSave}`,
+    JSON.stringify({ hour: hourToSave, task: textToSave })
+  );
 });
